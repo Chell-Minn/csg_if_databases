e987e1f8d245f1222dff81d5177034fe3ad0f449
Chell-Minn <151285@csgaugustinus.nl>
2021-03-24 11:07:08 +0000

    Opdracht 3, klaar

--- public/PHP/opdracht_3.php
+++ public/PHP/opdracht_3.php
@@ -9,9 +9,18 @@ $waarde=9/10;
 $kwad=pow($waarde,2);
 $rec=1/$kwad;
 $afgerond=round($rec,7);
+$afgekapt=floor(1000000*$rec)/1000000;
 
 echo "Het kwadraat van $waarde is $kwad.<br>
-      Het omgekeerde daarvan is (afgerond:) $afgerond<br>";
+      Het omgekeerde daarvan is (afgerond:) $afgerond<br>
+      Afgekapt is de waarde veel mooier: $afgekapt<br>";
+
+$a=3;
+$b=4;
+$c=round(sqrt(pow($a,2)+pow($b,2)),3);
+echo "Een driehoek met rechte zijden $a en $b heeft als schuine zijde (afgerond) $c<br>";
+$mod=fmod($b,$a);
+echo "De restwaarde van de deling $b / $a is $mod";
 
 /****************************
 EINDE VAN JOUW PHPCODE
