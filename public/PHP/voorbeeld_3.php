62a8264abd1ee5e18a62f52aaedf5e28ddd40c3d
Chell-Minn <151285@csgaugustinus.nl>
2021-03-29 10:37:10 +0000

    Mmmm

--- public/PHP/voorbeeld_3.php
+++ public/PHP/voorbeeld_3.php
@@ -8,7 +8,7 @@
       <?php
       // aanmaken van een array
       $priem=array(2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113);
-      echo $priem[1]."<br>";                       // standaard verwijs je naar een element met een nummer, beginnend bij 0
+      echo $priem[1]."<br>";                          // standaard verwijs je naar een element met een nummer, beginnend bij 0
       
       $namen=array();                                 // een lege array
       array_push($namen,"Martin","Marije");           // een array aanvullen (aan het eind)
