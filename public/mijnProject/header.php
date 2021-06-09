23cfbb48d402ccab25cf1bdbab2762c7193335ec
Chell-Minn <151285@csgaugustinus.nl>
2021-06-09 10:06:48 +0000

    99+ changes

--- public/mijnProject/header.php
+++ public/mijnProject/header.php
@@ -1,19 +1,8 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
-require('php/database.php');
-
-//maak databaseverbinding met de gegevens uit database.php
-$DBverbinding = mysqli_connect($servernaam, $gebruikersnaam, $wachtwoord, $database);
-// Controleer de verbinding
-if (!$DBverbinding) {
-// Geef de foutmelding die de server teruggeeft en stop met de uitvoer van PHP (die)
-die("Verbinding mislukt: " . mysqli_connect_error());
-}
-else {
-// Dit gedeelte laat je normaliter weg, maar is hier ter illustratie toegevoegd
-//echo '<i>verbinding database succesvol</i>';
-}
+require('database.php');
 ?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -22,26 +11,26 @@ else {
         <link rel="icon" href="https://static.planetminecraft.com/files/resource_media/screenshot/1321/Creeper-the-minecraft-creeper-32729200-1200-1200_5515445_thumb.jpg">
     </head>
     <body>
-        <div id="header">
+    <div id="header">
 
 <div class="navbar">
   <a href="index.php">Home</a>
-  <a href="uitleg.php">Uitleg</a>
   <div class="dropdown">
     <button class="dropbtn">Speedruns
       <i class="fa fa-caret-down"></i>
     </button>
     <div class="dropdown-content">
-      <a href="Speedruns.php">Enderdragon Random Seed%</a>
-      <a href="Speedruns.php">Enderdragon Set Seed%</a>
-      <a href="Speedruns.php">All achievements%</a>
-      <a href="Speedruns.php">Enderdragon Random Seed Glitchless%</a>
-      <a href="Speedruns.php">Enderdragon Set Seed Glitchless%</a>
-      <a href="Speedruns.php">Eat all food%</a>
-      <a href="Speedruns.php">Border%</a>
-      <a href="Speedruns.php">Mushroom Biome%</a>
-      <a href="Speedruns.php">Obtain All Blocks%</a>
-      <a href="Speedruns.php">Mine A Chunk</a>
+      <a href="Enderdragon_Random_Seed.php">Enderdragon Random Seed%</a>
+      <a href="Enderdragon_Set_Seed.php">Enderdragon Set Seed%</a>
+      <a href="All_achievements.php">All achievements%</a>
+      <a href="Enderdragon_Random_Seed_Glitchless.php">Enderdragon Random Seed Glitchless%</a>
+      <a href="Enderdragon_Set_Seed_Glitchless.php">Enderdragon Set Seed Glitchless%</a>
+      <a href="Eat_all_food.php">Eat all food%</a>
+      <a href="Border.php">Border%</a>
+      <a href="Mushroom_Biome.php">Mushroom Biome%</a>
+      <a href="Obtain_All_Blocks.php">Obtain All Blocks%</a>
+      <a href="Mine_A_Chunk.php">Mine A Chunk</a>
     </div>
   </div>
+ </div>
 </div>
--- public/mijnProject/index.php
+++ public/mijnProject/index.php
@@ -1,38 +1,12 @@
 <?php
 error_reporting(E_ALL & ~E_NOTICE);
-require('php/database.php');
-require('php/header.php');
-require('php/footer.php');
+include('database.php');
 
-//maak databaseverbinding met de gegevens uit database.php
-$DBverbinding = mysqli_connect($servernaam, $gebruikersnaam, $wachtwoord, $database);
-// Controleer de verbinding
-if (!$DBverbinding) {
-// Geef de foutmelding die de server teruggeeft en stop met de uitvoer van PHP (die)
-die("Verbinding mislukt: " . mysqli_connect_error());
-}
-else {
-// Dit gedeelte laat je normaliter weg, maar is hier ter illustratie toegevoegd
-//echo '<i>verbinding database succesvol</i>';
-}
 ?>
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Official MineCraft Spedrun</title>
-        <link rel="stylesheet" type="text/css" href="css/design.css">
-        <link rel="icon" href="https://static.planetminecraft.com/files/resource_media/screenshot/1321/Creeper-the-minecraft-creeper-32729200-1200-1200_5515445_thumb.jpg">
-    </head>
-    <body>
+    <?php include('header.php');?>
         <div id="container">
             <h1>
                 <?php echo 'Welcome to the official MineCraft Spedrun website';?>
             </h1> 
         </div>
-        <div id="footer">
-            <h1>
-                <?php echo 'contactgegevens hier';?>
-            </h1>
-        </div>
-    </body>
-</html>
+    <?php include('footer.php');?>
