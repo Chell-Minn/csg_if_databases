9d5d0c84bd53360b95203227072975e71982bfd8
Chell-Minn <151285@csgaugustinus.nl>
2021-06-23 16:05:33 +0000

    Progressie

--- public/mijnProject/php/submit_speedrun.php
+++ public/mijnProject/php/submit_speedrun.php
@@ -7,6 +7,7 @@ if (isset($_POST['submit-speedrun']) && isset($_SESSION["userId"])) {
     $categoryId = $_POST['category-id'];
     $name = $_SESSION['userName'];
     $time = $_POST['time'];
+    $proof  = $_POST['proof'];
 
     if (empty($categoryId) || empty($time)) {
         header("Location: ../submit.php?error");
@@ -14,15 +15,21 @@ if (isset($_POST['submit-speedrun']) && isset($_SESSION["userId"])) {
     } else {
         $query = "SELECT categorie_id FROM categorie WHERE categorie_id=" . $categoryId;
         if ($result = $mysqli->query($query)) {
-            
-            // Insert
-            $dt = new DateTime();
-            $timestamp = $dt->getTimestamp();
-            $query = "INSERT INTO speedruns (categorie_id, naam, tijd, datum) VALUES ('$categoryId', '$name', '$time', FROM_UNIXTIME($timestamp))";
-            if ($mysqli->query($query)) {
-                header("Location: ../submit.php?succes");
+            if ($result->num_rows == 1) {
+                $categorieRow = mysqli_fetch_array($result);
+                $result->free_result();
+
+                // Insert
+                $dt = new DateTime();
+                $timestamp = $dt->getTimestamp();
+                $query = "INSERT INTO speedruns (categorie_id, naam, tijd, datum, bewijs) VALUES ('$categoryId', '$name', '$time', FROM_UNIXTIME($timestamp), '$proof')";
+                if ($result = $mysqli->query($query)) {
+                    header("Location: ../speedrun.php?c=".$categoryId);
+                } else {
+                    header("Location: ../submit.php?error=sql+query=".$query);
+                }
             } else {
-                header("Location: ../submit.php?error");
+                header("Location: ../submit.php?error=rows");
             }
         } else {
             header("Location: ../submit.php?error=invalidCategory");
