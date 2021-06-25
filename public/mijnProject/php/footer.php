0218117003ebb11b982dd5adc66805a9aa75183d
Chell-Minn <151285@csgaugustinus.nl>
2021-06-25 12:15:45 +0000

    Done I guess

--- public/mijnProject/php/footer.php
+++ public/mijnProject/php/footer.php
@@ -1,5 +1,5 @@
 <div id="footer">
-    &copy; Copyright 2021 &middot; MineCraft SpedRun Inc. &middot; All rights reserved, official website
+    &copy; Copyright 2021 &middot; MineCraft SpedRun Inc. &middot; All rights reserved, official website.
 </div>
 </body>
 
